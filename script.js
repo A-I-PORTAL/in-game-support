84d4fccdb04bcf637e5e4ea14d99b670868cebe1
A-I-PORTAL <159739004+A-I-PORTAL@users.noreply.github.com>
2024-06-02 16:07:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBTalPVVePU959aKRxiy145EanVc6ob3dI",
-    authDomain: "gamer-support-394c1.firebaseapp.com",
-    projectId: "gamer-support-394c1",
-    storageBucket: "gamer-support-394c1.appspot.com",
-    messagingSenderId: "752562072624",
-    appId: "1:752562072624:web:c2cc3560845541e43a91f5",
-    measurementId: "G-THD3RTE4NC"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// script.js
 
 // Initialize FirebaseUI
 const uiConfig = {
@@ -79,6 +67,7 @@ postBtn.addEventListener('click', () => {
         postInput.value = '';
     }
 });
+
 
 
 
