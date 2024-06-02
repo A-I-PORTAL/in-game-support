7352399253e31c3448ca92093a188117e19ba56a
A-I-PORTAL <159739004+A-I-PORTAL@users.noreply.github.com>
2024-06-02 16:02:00 -0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBTalPVVePU959aKRxiy145EanVc6ob3dI",
+    authDomain: "gamer-support-394c1.firebaseapp.com",
+    projectId: "gamer-support-394c1",
+    storageBucket: "gamer-support-394c1.appspot.com",
+    messagingSenderId: "752562072624",
+    appId: "1:752562072624:web:c2cc3560845541e43a91f5",
+    measurementId: "G-THD3RTE4NC"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
